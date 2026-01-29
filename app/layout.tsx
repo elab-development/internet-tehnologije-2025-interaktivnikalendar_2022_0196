2b01475904fb28855e6ae06de501785662167045
Bojana <bojanaraonic@gmail.com>
2026-01-28 21:04:23 -0800

    Dodata nova komponenta Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +27,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#fac7d0] min-h-screen flex flex-col`}
       >
         <Navbar />
-        {children}
+        
+        <main className="flex-1 min-h-screen">
+         
+          {children}
+          
+        </main>
+        <Footer />
       </body>
+      
     </html>
   );
 }
