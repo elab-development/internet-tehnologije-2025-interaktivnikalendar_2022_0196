955484b2c4c5db78287b0a629a9427e238eea7f4
Bojana <bojanaraonic@gmail.com>
2026-02-23 20:14:35 -0800

    Dodata CORS zastita za API rute

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // samo za API rute
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000/",  //samo zahtevi sa localhosta mogu da idu na api rute
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",  //koji headersi smeju da se salju u zahtevu
+          },
+          // Kolacici se šalju samo sa istog domena
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
