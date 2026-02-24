e405ce4bcb022a87957d57f6fa2c9e20b71258c0
teodora <teamikic18@gmail.com>
2026-02-24 18:40:35 +0100

    dodat SSl za Avien konekciju

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,11 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({ uri: process.env.DATABASE_URL });
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
 
 export const db = drizzle({ client: pool });
