3bb057b7aeb5e9acb2f9f4c0e95ace4040759c6d
teodora <teamikic18@gmail.com>
2026-02-24 21:05:19 +0100

    fixed jest

--- jest.config.js.ts
+++ jest.config.js.ts
@@ -1,20 +1,11 @@
 /** @type {import('jest').Config} */
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
-  testMatch: [
-    "**/__tests__/**/*.test.ts",
-  ],
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: {
-          module: "commonjs",
-        },
-      },
-    ],
-  },
+
+  testMatch: ["**/__tests__/**/*.test.ts"],
 };
